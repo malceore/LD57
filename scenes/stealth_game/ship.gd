931ab96fb8f95e9a4e4fa0b6f99cb8ad9d741cae
Bob Slob <zezoku@yahoo.com>
2025-04-05 11:30:59 -0400

    fixed ship rotation

--- scenes/stealth_game/ship.gd
+++ scenes/stealth_game/ship.gd
@@ -8,16 +8,17 @@ func _process(delta):
 	var rotate_direction = 0.0
 
 	if Input.is_action_pressed("up"):
-		move_direction += Vector2(0, -1)
+		move_direction += Vector2.UP
 	if Input.is_action_pressed("down"):
-		move_direction += Vector2(0, 1)
+		move_direction += Vector2.DOWN
 	if Input.is_action_pressed("left"):
 		rotate_direction -= 1.0
 	if Input.is_action_pressed("right"):
 		rotate_direction += 1.0
 
-	velocity = move_direction.normalized() * speed
-	rotate_direction *= rotation_speed
+	rotation += rotate_direction * rotation_speed * delta
 
-	rotation += rotate_direction * delta
+	move_direction = move_direction.rotated(rotation)
+	velocity = move_direction.normalized() * speed
+
 	move_and_slide()
