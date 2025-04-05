18aebe62be58cdd4c830791808a286b629246deb
b <malceore@gmail.com>
2025-04-05 17:45:40 -0400

    no longer rezise screen and added fish spawn that give you points.

---
+++ scenes/fish_spawner/fish_spawner.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@export var direction: int = 1
+@export var spawn_rate: float = 1.0
+@export var fish_scene: PackedScene
+@export var spawn_points: Path2D
+
+
+func _ready() -> void:
+	$timer.wait_time = spawn_rate
+	$timer.start()
+
+func _on_timer_timeout() -> void:
+	spawn_fish_along_path()
+
+func spawn_fish_along_path():
+	var curve = spawn_points.curve
+	var points = curve.get_baked_points()
+	var random_index = randi() % points.size()
+	var spawn_point = points[random_index]
+	var new_fish = fish_scene.instantiate()
+	new_fish.position = spawn_point
+	new_fish.direction = direction
+	add_child(new_fish)
