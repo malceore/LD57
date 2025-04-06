ef138d42369985f1218e162669aefb9a07511381
b <malceore@gmail.com>
2025-04-05 20:46:15 -0400

    stitching up separated scenes and trying to get back out to stealth game.

--- scenes/main/main_game.gd
+++ scenes/main/main_game.gd
@@ -4,14 +4,20 @@ extends Node2D
 
 var dive_spots = [null, preload("res://scenes/dive_game/dive_game.tscn")]
 
+var current_dive_spot = null
+
 func _on_title_screen_start_game_pressed() -> void:
 	$title_screen.hide()
 	$stealth_game/Camera2D.make_current()
 	$stealth_game.show()
 
-
 func _on_stealth_game_dive(location) -> void:
-	var dive_spot_current = dive_spots[location].instantiate()
-	get_tree().root.add_child(dive_spot_current)
-	dive_spot_current.get_node("hook").get_node("hook").get_node("Camera2D").make_current()
+	current_dive_spot = dive_spots[location].instantiate()
+	get_tree().root.add_child(current_dive_spot)
+	current_dive_spot.get_node("hook").get_node("hook").get_node("Camera2D").make_current()
 	remove_child($stealth_game)
+
+func _on_dive_game_end_game_pressed() -> void:
+	stealth_game_scene.get_node("Camera2D").make_current()
+	current_dive_spot.queue_free()
+	get_tree().root.add_child(stealth_game_scene)
--- scenes/ship_return/ship_return.gd
+++ scenes/ship_return/ship_return.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @onready var score_float_text = get_node("float_text")
 @onready var colShape2D = get_node("CollisionShape2D")
-@onready var set_sail_button = get_node("Button")
+@onready var set_sail_button = get_node("SetSailButton")
 
 var enabled = false
 
@@ -16,3 +16,6 @@ func _on_body_entered(body:Node2D) -> void:
 	if enabled:
 		score_float_text.reveal("<your score here>")
 		set_sail_button.visible = true
+
+func _on_set_sale_button_pressed() -> void:
+	get_tree().get_root().get_node("main_game")._on_dive_game_end_game_pressed()
